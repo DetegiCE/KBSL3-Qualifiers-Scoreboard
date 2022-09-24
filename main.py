dd41ec4793d946540c177d83379aa7260ad6d3fb
DetegiCE <martinok1103@naver.com>
2022-09-24 17:00:00 +0900

    edit datas for kbsl 3

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from getScores import getScores
 from makeJson import generateJSON
 
 if __name__ == '__main__':
-    for grade in range(1, 4):
+    for grade in range(1, 5):
         generateJSON(grade, getScores(grade))

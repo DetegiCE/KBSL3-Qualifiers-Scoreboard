f98cba6e95e850c25f3ce02e0454f6a2941e3cfd
RBK <martinok1103@naver.com>
2022-09-27 21:01:56 +0900

    change rank calc system

--- getScores.py
+++ getScores.py
@@ -32,6 +32,25 @@ def getScoresBySong(hash: str, diff: str, grade: int) -> dict:
         for s in rjson['scores']:
             if s['leaderboardPlayerInfo']['id'] in ssidList:
                 scores[s['leaderboardPlayerInfo']['id']] = s['baseScore']
+                count -= 1
+        if count == 0:
+            break
+    return scores
+
+def getMissesBySong(hash: str, diff: str, grade: int) -> dict:
+    player = getPlayerList(grade)
+    count = len(player['player'])
+    ssidList = list()
+    scores = dict()
+    for p in player['player']:
+        ssidList.append(p['ssid'])
+    r = requests.get(f'https://scoresaber.com/api/leaderboard/by-hash/{hash}/scores?difficulty={getDiff(diff)}&countries=KR,JP')
+    totalPages = ((r.json()['metadata']['total']-1) // r.json()['metadata']['itemsPerPage'])+1
+    for page in range(1, totalPages+1):
+        r = requests.get(f'https://scoresaber.com/api/leaderboard/by-hash/{hash}/scores?difficulty={getDiff(diff)}&countries=KR,JP&page={page}')
+        rjson = r.json()
+        for s in rjson['scores']:
+            if s['leaderboardPlayerInfo']['id'] in ssidList:
                 scores[s['leaderboardPlayerInfo']['id']+"M"] = s['missedNotes'] + s['badCuts']
                 count -= 1
         if count == 0:
@@ -55,11 +74,12 @@ def getScores(grade: int):
     player = getPlayerList(grade)
     for s in song['songs']:
         sdict = getScoresBySong(s['hash'], s['diff'], grade)
+        mdict = getMissesBySong(s['hash'], s['diff'], grade)
         for p in player['player']:
             if p['ssid'] in sdict:
                 p[s['code']] = getAcc(s['note'], sdict[p['ssid']])
                 p[s['code']+'R'] = calcRank(sdict[p['ssid']], sdict)
-                p[s['code']+'M'] = sdict[p['ssid']+'M']
+                p[s['code']+'M'] = mdict[p['ssid']+'M']
             else:
                 p[s['code']] = 0
                 p[s['code']+'R'] = calcRank(0, sdict)
